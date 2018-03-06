0648aca671fc4c147ccc20fb9e15b60cd92cbfe6
Mara Childs <marachilds@Maras-MacBook-Pro.local>
2018-03-06 15:16:44 +0000

    clarified variable names

--- topTracks2017.R
+++ topTracks2017.R
@@ -10,28 +10,24 @@ setwd("~/Google Drive/School/GSA 2017-2018/7-Data Visualization/dataviz")
 tracks <- read.csv("data/toptracks2017.csv", stringsAsFactors = FALSE)
 
 # Tracks by ascending duration
-tracks_asc <- arrange(tracks, duration_ms) %>% select(name, duration_ms)
+duration_asc <- arrange(tracks, duration_ms) %>% select(name, duration_ms)
+
+# Add average duration in ms
+duration_avg <- duration_asc %>% 
+  summarise(name = "Average Length",
+            duration_ms = mean(duration_ms)) %>% 
+  bind_rows(duration_asc)
 
 # Set factor level order for top-bottom ascending duration
-tracks_asc$y <- factor(tracks_asc$name, levels = unique(tracks_asc$name)[order(tracks_asc$duration_ms, decreasing = TRUE)])
+duration_avg$y <- factor(duration_avg$name, levels = unique(duration_avg$name)[order(duration_avg$duration_ms, decreasing = TRUE)])
 
-# Alternatively, set row number to column called number
-# df_names <- rownames_to_column(tracks_asc, "number")
+# Alternatively, set row number to column called number to replace "~y"
+# tracks_asc <- rownames_to_column(tracks_asc, "number")
 
-p1 <- plot_ly(tracks_asc, type="bar", 
+p1 <- plot_ly(duration_avg, type="bar", 
               orientation="h", 
               x = ~duration_ms, 
               y = ~y)
 plotly_build(p1)
 
-# Add average duration in ms
-tracks_asc %>% 
-    summarise(name = "allAverage",
-              duration_ms = mean(duration_ms)) %>% 
-bind_rows(tracks_asc)
 
-data %<>%
-  summarise(cvar = "add",
-            nvar1 = sum(nvar1),
-            nvar2 = sum(nvar2)) %>%
-  bind_rows(data)
