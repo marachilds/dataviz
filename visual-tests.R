8667613810e858df8da06e63d9200f58a0800c13
Mara Childs <marachilds@Maras-MacBook-Pro.local>
2018-03-06 10:32:37 +0000

    initial tests

---
+++ visual-tests.R
@@ -0,0 +1,30 @@
+# Load dependencies
+library(plotly)
+library(dplyr)
+
+# Use state data as example data set
+df <- as.data.frame(state.x77)
+
+# Create plotly object
+p <- plot_ly(df, type="scatter", mode="markers", x = ~Population, y = ~Income, color = ~Illiteracy)
+
+# Build plotly object
+plotly_build(p)
+
+# Make state names column 1
+df_names <- add_rownames(df, "State")
+
+# Remove spaces from columns
+names(df_names)[4] <- "LifeExp"
+names(df_names)[6] <- "HSGrad"
+
+# Create scatter plot with trace
+p_trace <- plot_ly(df_names, type="scatter", mode="markers",
+                   x = ~Population,
+                   y = ~Income,
+                   text = df_names$State,
+                   color = ~Illiteracy)
+
+# Build plot
+plotly_build(p_trace)
+
