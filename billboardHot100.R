ec1c5e44aae21f4c0c4aa955a2da24118d60408a
Mara Childs <marachilds@gmail.com>
2018-03-21 21:29:05 +0000

    your girl forgot she had other files with camelCase still wandering around

--- billboardHot100.R
+++ billboardHot100.R
@@ -7,75 +7,75 @@ library(plotly)
 load(file = "data/spotify_track_data.rda")
 
 # Rename to allYears because I am high maintenance
-allYears <- spotify_track_data
+all_years <- spotify_track_data
 
 # Let's look at the extremes first
-year1960 <- filter(allYears, year == 1960)
-year2015 <- filter(allYears, year == 2015)
+year_1960 <- filter(all_years, year == 1960)
+year_2015 <- filter(all_years, year == 2015)
 
 # There is a huge difference, as to be expected
-summary1960 <- year1960 %>% 
-               select(danceability, energy, loudness, speechiness, acousticness,
+summary_1960 <- year_1960 %>% 
+                select(danceability, energy, loudness, speechiness, acousticness,
                       instrumentalness, liveness, valence, tempo) %>%
-               summarise_all(funs(mean))
+                summarise_all(funs(mean))
                         
-summary2015 <- year2015 %>% 
+summary_2015 <- year_2015 %>% 
                select(danceability, energy, loudness, speechiness, acousticness,
                       instrumentalness, liveness, valence, tempo) %>%
                summarise_all(funs(mean))
 
 # Differences between 1960 and 2015
-df1960 <- gather(summary1960, "metric", "year1960")
-df2015 <- gather(summary2015, "metric", "year2015")
-rangePastToPresent <- left_join(df1960, df2015, by = "metric") %>% 
+df_1960 <- gather(summary_1960, "metric", "year1960")
+df_2015 <- gather(summary_2015, "metric", "year2015")
+past_to_present <- left_join(df_1960, df_2015, by = "metric") %>% 
                       mutate(change = year2015 - year1960)
 
 # Aggregate examination
-allAvg <- allYears %>% group_by(year) %>% summarise_all(mean)
+all_avg <- all_years %>% group_by(year) %>% summarise_all(mean)
 
 # Use to write averages to CSV for Processing
-write.csv(allAvg, file = "billboardHot100.csv")
+write.csv(all_avg, file = "billboardHot100.csv")
 
 # Explicitness
-explicitBar <- plot_ly(allAvg, x = ~year, y = ~explicit, type = "bar")
-plotly_build(explicitBar)
+explicit_bar <- plot_ly(all_avg, x = ~year, y = ~explicit, type = "bar")
+plotly_build(explicit_bar)
 
 # Danceability
-danceBar <- plot_ly(allAvg, x = ~year, y = ~danceability, type = "bar")
-plotly_build(danceBar)
+dance_bar <- plot_ly(all_avg, x = ~year, y = ~danceability, type = "bar")
+plotly_build(dance_bar)
 
 # Energy
-energyBar <- plot_ly(allAvg, x = ~year, y = ~energy, type = "bar")
-plotly_build(energyBar)
+energy_bar <- plot_ly(all_avg, x = ~year, y = ~energy, type = "bar")
+plotly_build(energy_bar)
 
 # Loudness
-loudBar <- plot_ly(allAvg, x = ~year, y = ~loudness, type = "bar")
-plotly_build(loudBar)
+loud_bar <- plot_ly(all_avg, x = ~year, y = ~loudness, type = "bar")
+plotly_build(loud_bar)
 
 # Speechiness
-speechBar <- plot_ly(allAvg, x = ~year, y = ~speechiness, type = "bar")
-plotly_build(speechBar)
+speech_bar <- plot_ly(all_avg, x = ~year, y = ~speechiness, type = "bar")
+plotly_build(speech_bar)
 
 # Acousticness
-acoustBar <- plot_ly(allAvg, x = ~year, y = ~acousticness, type = "bar")
-plotly_build(acoustBar)
+acoust_bar <- plot_ly(all_avg, x = ~year, y = ~acousticness, type = "bar")
+plotly_build(acoust_bar)
 
 # Instrumentalness
-instBar <- plot_ly(allAvg, x = ~year, y = ~instrumentalness, type = "bar")
-plotly_build(instBar)
+instrumentalness_bar <- plot_ly(all_avg, x = ~year, y = ~instrumentalness, type = "bar")
+plotly_build(instrumentalness_bar)
 
 # Liveness
-liveBar <- plot_ly(allAvg, x = ~year, y = ~liveness, type = "bar")
-plotly_build(liveBar)
+liveness_bar <- plot_ly(all_avg, x = ~year, y = ~liveness, type = "bar")
+plotly_build(liveness_bar)
 
 # Valence
-valBar <- plot_ly(allAvg, x = ~year, y = ~valence, type = "bar")
-plotly_build(valBar)
+valence_bar <- plot_ly(all_avg, x = ~year, y = ~valence, type = "bar")
+plotly_build(valence_bar)
 
 # Tempo
-tempoBar <- plot_ly(allAvg, x = ~year, y = ~tempo, type = "bar")
-plotly_build(tempoBar)
+tempo_bar <- plot_ly(all_avg, x = ~year, y = ~tempo, type = "bar")
+plotly_build(tempo_bar)
 
 # Duration
-durationBar <- plot_ly(allAvg, x = ~year, y = ~duration_ms, type = "bar")
-plotly_build(durationBar)
+duration_bar <- plot_ly(all_avg, x = ~year, y = ~duration_ms, type = "bar")
+plotly_build(duration_bar)
